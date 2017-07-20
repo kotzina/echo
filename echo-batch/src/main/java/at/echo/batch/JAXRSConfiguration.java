40600ae899b17dce33030612a5141d02ffc2a8de
kotzina <oliver.kotzina@gmail.com>
2017-07-20 09:15:11 +0200

    Setting up .gitignore, changed package names

--- echo-batch/src/main/java/at/echo/batch/JAXRSConfiguration.java
+++ echo-batch/src/main/java/at/echo/batch/JAXRSConfiguration.java
@@ -1,4 +1,4 @@
-package com.airhacks;
+package at.echo.batch;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- echo-web/src/main/java/at/echo/web/JAXRSConfiguration.java
+++ echo-web/src/main/java/at/echo/web/JAXRSConfiguration.java
@@ -1,4 +1,4 @@
-package com.airhacks;
+package at.echo.web;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
